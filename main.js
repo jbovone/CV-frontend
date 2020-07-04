929e2d7b97b6ae1c3f27471308f4962901f18ea2
jbovone <58782618+jbovone@users.noreply.github.com>
2020-07-04 05:54:16 -0300

    detalles responsividad añadida

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
 const proficiencies = {
-    "css":7, 
-    "html":6, 
-    "JavaScript":7, 
-    "Saas":6,
-    "Git":5, 
-    "Bootstrap":4,
-    "jQuery":4, 
-    "Node":4,
-    "Npm":4,
-    "Figma":4,
-    "React":3,
-    "HandleBars":3,
-    "Webpack":3,
-    "Cypress":3,
-    "Jest":2,
+    "Css":4, 
+    "Html":4, 
+    "JavaScript":4, 
+    "Saas":4,
+    "Git":2, 
+    "Bootstrap":2,
+    "jQuery":3, 
+    "Node":2,
+    "Npm":2,
+    "Figma":3,
+    "React":2,
+    "HandleBars":2,
+    "Webpack":2,
+    "Cypress":2,
+    "Jest":1,
     "PWA":1,
     "Materialize":1
 }
 const technologies = Object.keys(proficiencies)
 for (let i = 0; i < technologies.length; i++) {
+
     const $skillsContainer = document.createElement('li')
+    document.querySelector('main ol').appendChild($skillsContainer)
+    
     const $tagName = document.createElement('div')
     $tagName.textContent =  technologies[i]
     $skillsContainer.appendChild($tagName)
-    document.querySelector('main ol').appendChild($skillsContainer)
+    
+
     const $valuesContainer = document.createElement('div')
+    $valuesContainer.className = 'values-container'
     $skillsContainer.appendChild($valuesContainer)
-    const SKILL_LIMIT = 10
+
+    const SKILL_LIMIT = 5
     let assign = 0
-    while (assign <= SKILL_LIMIT) {
+    while (assign < SKILL_LIMIT) {
         const $value = document.createElement('div')
         $valuesContainer.appendChild($value)
         if(assign < proficiencies[technologies[i]]){
-         $value.className = 'active'
+         $value.className = 'skill active'
         } else{
-            $value.className = 'inactive'
+            $value.className = 'skill inactive'
         }
         assign++
     }
